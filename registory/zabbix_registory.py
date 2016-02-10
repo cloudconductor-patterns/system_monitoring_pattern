026ff87d58e2c8bb60b80c7c72026f5769a62fea
bsmile <battlesmile@gmail.com>
2016-02-10 14:10:39 +0900

    Change method cloudconductor token delivery

--- registory/zabbix_registory.py
+++ registory/zabbix_registory.py
@@ -151,7 +151,6 @@ if __name__ == '__main__':
     decode_consul_kv = json.loads(consul_kv)
     decode_user_attribute = json.loads(base64.b64decode(decode_consul_kv[0]["Value"]))
     cloudconductor_url = decode_user_attribute['common']['cloudconductor_url']
-    cloudconductor_token = decode_user_attribute['common']['cloudconductor_token']
     zabbix_user = decode_user_attribute['zabbix_part']['zabbix_user']
     zabbix_password = decode_user_attribute['zabbix_part']['zabbix_password']
     zabbix_url = decode_user_attribute['zabbix_part']['zabbix_url']
@@ -162,6 +161,11 @@ if __name__ == '__main__':
     decode_consul_kv = json.loads(consul_kv)
     decode_user_attribute = json.loads(base64.b64decode(decode_consul_kv[0]["Value"]))
     environment_id = decode_user_attribute['environment_id']
+
+    consul_kv = get_kv("cloudconductor/account_token",ret_url_encode(os.environ['CONSUL_SECRET_KEY']))
+    decode_consul_kv = json.loads(consul_kv)
+    decode_user_attribute = json.loads(base64.b64decode(decode_consul_kv[0]["Value"]))
+    cloudconductor_token = decode_user_attribute['auth_token']
 
     consul_kv = get_kv("cloudconductor/system_domain",ret_url_encode(os.environ['CONSUL_SECRET_KEY']))
     decode_consul_kv = json.loads(consul_kv)
